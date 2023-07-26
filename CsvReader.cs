7e01d559e213e3b0327957128951c856483c8a03
FOp42 <140609124+FOp42@users.noreply.github.com>
2023-07-26 22:17:32 +0200

    Initial commit

---
+++ CsvReader.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using System.Collections;
+
+namespace FOp.CsvReader
+{
+class CsvReader
+{
+    private readonly StreamReader FInStream;
+    private readonly char FSeparator;
+    private readonly char FQuote;
+    private char[] FBuffer;
+    private int FBufferFill;
+    private int FBufferIndex;
+
+    private bool IsNewLine(char c)
+    {
+        if ((c >= ' ') && (c < '\x007f'))
+        {
+            return (false);
+        }
+        if ((c == '\n') || (c == '\r'))
+        {
+            return (true);
+        }
+        return (CharUnicodeInfo.GetUnicodeCategory(c) == UnicodeCategory.LineSeparator);
+    }
+
+    public CsvReader(StreamReader InStream, Char Separator = ',', Char Quote = '"')
+    {
+        if (IsNewLine(Separator))
+        {
+            throw new Exception("Line separator as separator not allowed");
+        }
+        if (IsNewLine(Quote))
+        {
+            throw new Exception("Line separator as quotes not allowed");
+        }
+        if (Separator == Quote)
+        {
+            throw new Exception("Separator equals quotes");
+        }
+        this.FInStream = InStream;
+        this.FSeparator = Separator;
+        this.FQuote = Quote;
+        this.FBuffer = new char[8192];
+        this.FBufferFill = 0;
+        this.FBufferIndex = 0;
+    }
+
+    private bool Read(out char derda)
+    {
+        if (this.FBufferIndex < this.FBufferFill)
+        {
+            derda = this.FBuffer[this.FBufferIndex];
+            this.FBufferIndex++;
+            return (true);
+        }
+        else
+        {
+            if (this.FInStream.EndOfStream)
+            {
+                derda = '\0';
+                return (false);
+            }
+            this.FBufferFill = this.FInStream.Read(this.FBuffer, 0, this.FBuffer.Length);
+            if (0 == this.FBufferFill)
+            {
+                derda = '\0';
+                return (false);
+            }
+            derda = this.FBuffer[0];
+            this.FBufferIndex = 1;
+            return (true);
+        }
+    }
+
+    private bool Peek(out char derda)
+    {
+        if (this.FBufferIndex < this.FBufferFill)
+        {
+            derda = this.FBuffer[this.FBufferIndex];
+            return (true);
+        }
+        else
+        {
+            if (this.FInStream.EndOfStream)
+            {
+                derda = '\0';
+                return (false);
+            }
+            this.FBufferFill = this.FInStream.Read(this.FBuffer, 0, this.FBuffer.Length);
+            if (0 == this.FBufferFill)
+            {
+                derda = '\0';
+                return (false);
+            }
+            derda = this.FBuffer[0];
+            this.FBufferIndex = 0;
+            return (true);
+        }
+    }
+
+    public bool NotEndOfStream()
+    {
+        if (this.FBufferIndex < this.FBufferFill)
+        {
+            return (true);
+        }
+        else
+        {
+            if (this.FInStream.EndOfStream)
+            {
+                return (false);
+            }
+            this.FBufferFill = this.FInStream.Read(this.FBuffer, 0, this.FBuffer.Length);
+            if (0 == this.FBufferFill)
+            {
+                return (false);
+            }
+            this.FBufferIndex = 0;
+            return (true);
+        }
+    }
+
+    public String[] ReadLine()
+    {
+        Boolean Quoted;
+        ArrayList Result;
+        StringBuilder SoFar = new StringBuilder();
+        char ach;
+        char dann;
+        bool doit = false;
+        Quoted = false;
+        Result = new ArrayList();
+        while (Read(out ach))
+        {
+            doit = true;
+            if (Quoted)
+            {
+                if (ach == this.FQuote)
+                {
+                    if (Peek(out dann))
+                    {
+                        if (dann == this.FQuote)
+                        {
+                            if (Read(out dann))
+                            {
+                                SoFar.Append(dann);
+                            }
+                        }
+                        else
+                        {
+                            Quoted = false;
+                        }
+                    }
+                    else
+                    {
+                        Quoted = false;
+                    }
+                }
+                else
+                {
+                    SoFar.Append(ach);
+                }
+            }
+            else
+            {
+                if (ach == this.FQuote)
+                {
+                    Quoted = true;
+                }
+                else
+                {
+                    if ((ach == this.FSeparator) || IsNewLine(ach))
+                    {
+                        Result.Add(SoFar.ToString());
+                        if (ach != this.FSeparator)
+                        {
+                            /* Zeilenende */
+                            if (Peek(out dann))
+                            {
+                                if (dann != ach)
+                                {
+                                    if (IsNewLine(dann))
+                                    {
+                                        Read(out dann);
+                                    }
+                                }
+                            }
+                            doit = false;
+                            break;
+                        }
+                        SoFar.Clear();
+                    }
+                    else
+                    {
+                        SoFar.Append(ach);
+                    }
+                }
+            }
+        }
+        if (doit)
+        {
+            Result.Add(SoFar.ToString());
+        }
+        return ((String[])(Result.ToArray(typeof(string))));
+    }
+}
+}
---
+++ CsvWriter.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FOp.CsvWriter
+{
+class CsvWriter : IDisposable
+{
+    private StreamWriter _outstream = null;
+    private readonly char _separator;
+    private readonly char _quote;
+
+    public CsvWriter(string filename, char separator = ';', char quote = '"', Encoding encoding = null, string lineending = "\r\n")
+    {
+        if (String.IsNullOrWhiteSpace(filename))
+        {
+            throw new ArgumentNullException("filename");
+        }
+        if (null == encoding)
+        {
+            encoding = Encoding.UTF8;
+        }
+        if (separator == quote)
+        {
+            throw new ArgumentException("separator equal to quote");
+        }
+        _outstream = new StreamWriter(filename, false, encoding)
+        {
+            NewLine = lineending
+        };
+        _separator = separator;
+        _quote = quote;
+    }
+
+    public CsvWriter(Stream stream, char separator = ';', char quote = '"', Encoding encoding = null, string lineending = "\r\n")
+    {
+        if (null == stream)
+        {
+            throw new ArgumentNullException("stream");
+        }
+        if (!stream.CanWrite)
+        {
+            throw new ArgumentException("stream not writeable");
+        }
+        if (null == encoding)
+        {
+            encoding = Encoding.UTF8;
+        }
+        if (separator == quote)
+        {
+            throw new ArgumentException("separator equal to quote");
+        }
+        _outstream = new StreamWriter(stream, encoding)
+        {
+            NewLine = lineending
+        };
+        _separator = separator;
+        _quote = quote;
+    }
+
+#if NET45_OR_GREATER || NETSTANDARD
+    public CsvWriter(Stream stream, bool leaveopen = false, char separator = ';', char quote = '"', Encoding encoding = null, string lineending = "\r\n")
+    {
+        if (null == stream)
+        {
+            throw new ArgumentNullException("stream");
+        }
+        if (!stream.CanWrite)
+        {
+            throw new ArgumentException("stream not writeable");
+        }
+        if (null == encoding)
+        {
+            encoding = Encoding.UTF8;
+        }
+        if (separator == quote)
+        {
+            throw new ArgumentException("separator equal to quote");
+        }
+        _outstream = new StreamWriter(stream, encoding, 4096, leaveopen)
+        {
+            NewLine = lineending
+        };
+        _separator = separator;
+        _quote = quote;
+    }
+#endif
+
+    public void Close()
+    {
+        if (_outstream != null)
+        {
+            _outstream.Close();
+            _outstream = null;
+        }
+    }
+
+    public void Dispose()
+    {
+        Close();
+    }
+
+    public void WriteLine(IEnumerable<string> data)
+    {
+        bool first = true;
+        string eins = new string(_quote, 1);
+        string zwei = new string(_quote, 2);
+        foreach (string element in data)
+        {
+            if (first)
+            {
+                first = false;
+            }
+            else
+            {
+                _outstream?.Write(_separator);
+            }
+            if (!String.IsNullOrEmpty(element))
+            {
+                bool mustquote = Char.IsWhiteSpace(element[0]) || Char.IsWhiteSpace(element[element.Length - 1]);
+                if (!mustquote)
+                {
+                    foreach (char c in element)
+                    {
+                        if ((c == _quote) || (c == _separator) || (_outstream.NewLine.IndexOf(c) > -1) || Char.IsControl(c))
+                        {
+                            mustquote = true;
+                            break;
+                        }
+                    }
+                }
+                if (mustquote)
+                {
+                    _outstream.Write(_quote);
+                    _outstream.Write(element.Replace(eins, zwei));
+                    _outstream.Write(_quote);
+                }
+                else
+                {
+                    _outstream.Write(element);
+                }
+            }
+        }
+        _outstream.WriteLine();
+    }
+
+    public void WriteLines(IEnumerable<IEnumerable<string>> data)
+    {
+        foreach (IEnumerable<string> line in data)
+        {
+            WriteLine(line);
+        }
+    }
+}
+}
